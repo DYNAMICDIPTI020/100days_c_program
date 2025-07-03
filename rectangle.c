2b092ba03dbc0da0c66e23fefa510efff7a72ef6
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-07-03 23:37:41 +0530

    this is a code find the area of a rectangle

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+int main()
+{
+	int area , height , width;
+	printf("Enter the height ");
+	scanf("%d", &height);	
+	printf("Enter the width ");
+	scanf("%d", &width);
+	area  = height * width ;
+	printf("the area of the rectagnle is %d", area);
+	
+}
